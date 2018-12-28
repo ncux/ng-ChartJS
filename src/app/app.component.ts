40e83ef162b7f5e4985edaba9784bb616815e522
Talent Malaba <malabatalent@gmail.com>
2018-12-28 16:40:38 +0800

    minor cleanup

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,60 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { WeatherService } from './weather.service';
+
+import { Chart } from 'chart.js';
+
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'app';
+export class AppComponent implements OnInit{
+
+  chart = [];
+
+  constructor(private weather: WeatherService) {}
+
+  ngOnInit() {
+    this.weather.dailyForecast().subscribe(res => {
+      console.log(res);
+      let tempMax = res['list'].map(res => res.main.temp_max);
+      let tempMin = res['list'].map(res => res.main.temp_min);
+      let allDates = res['list'].map(res => res.dt);
+
+      let weatherDates = [];
+
+      allDates.forEach(date => {
+        let newDate = new Date(date * 1000);
+        weatherDates.push(newDate.toLocaleTimeString('en', {year: 'numeric', month: 'short', day: 'numeric'}));
+      });
+
+      this.chart = new Chart('canvas', {
+        type: 'line',
+        data: {
+          labels: weatherDates,
+          datasets: [
+            {
+              data: tempMax,
+              borderColor: "#3cba9f",
+              fill: false
+            },
+            {
+              data: tempMin,
+              borderColor: "#ffcc00",
+              fill: false
+            }
+          ]
+        },
+        options: {
+          legend: { display: false },
+          scales: {
+            xAxes: [{ display: true }],
+            yAxes: [{ display: true }],
+          }
+        }
+      })
+
+    });
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,8 @@ import { NgModule } from '@angular/core';
 
 
 import { AppComponent } from './app.component';
+import {WeatherService} from './weather.service';
+import {HttpClientModule} from '@angular/common/http';
 
 
 @NgModule({
@@ -10,9 +12,10 @@ import { AppComponent } from './app.component';
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HttpClientModule
   ],
-  providers: [],
+  providers: [WeatherService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
